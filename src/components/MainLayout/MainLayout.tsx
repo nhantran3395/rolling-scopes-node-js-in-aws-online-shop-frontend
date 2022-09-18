7f0c83621f950a0c0103ce3955d0a1f63cd991e7
tranthanhnhan3395 <thanhnhan33.95@gmail.com>
2022-09-18 11:50:20 +0700

    finish task 2: set up s3 bucket and cloudfront distribution

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -1,19 +1,19 @@
-import React from "react";
-import Typography from "@mui/material/Typography";
-import Link from "@mui/material/Link";
-import Container from "@mui/material/Container";
-import Header from "~/components/MainLayout/components/Header";
-import Box from "@mui/material/Box";
+import React from 'react';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import Container from '@mui/material/Container';
+import Header from '~/components/MainLayout/components/Header';
+import Box from '@mui/material/Box';
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
+      {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/" underline="hover">
-        My Store
-      </Link>{" "}
+        Nhan's shop
+      </Link>{' '}
       {new Date().getFullYear()}
-      {"."}
+      {'.'}
     </Typography>
   );
 }
@@ -28,7 +28,7 @@ const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
         </Container>
       </main>
       <Box
-        component={"footer"}
+        component={'footer'}
         sx={{ bgcolor: (theme) => theme.palette.background.paper, padding: 6 }}
       >
         <Typography
@@ -37,7 +37,7 @@ const MainLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
           color="textSecondary"
           component="p"
         >
-          Thank you for your purchase!
+          Welcome to Nhan's shop!
         </Typography>
         <Copyright />
       </Box>
